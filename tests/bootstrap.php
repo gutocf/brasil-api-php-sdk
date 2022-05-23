5fc93d8a9842a10ccaffc1f946d7d1853ef1f78e
Augusto Cesar Ferreira <gutocf@gmail.com>
2022-05-23 10:14:07 -0300

    adds utility functions for testing

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,19 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 
 function loadFixture(string $path): array
 {
-    $filename = __DIR__ . '/Fixture/' . $path;
+    $filename = __DIR__ . '/Fixture/' . $path . '.json';
 
     return json_decode(file_get_contents($filename), true);
+}
+
+function pr($var)
+{
+    print_r($var);
+    echo "\n";
+}
+
+function prd($var)
+{
+    print_r($var);
+    die();
 }
