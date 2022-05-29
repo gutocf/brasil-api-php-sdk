0a1102f9cb3047b916698c8c6a8b60f010adeb9f
Augusto César Ferreira <gutocf@gmail.com>
2022-05-29 18:54:31 -0300

    updates adapter tests

--- tests/Adapter/AbstractAdapterTest.php
+++ tests/Adapter/AbstractAdapterTest.php
@@ -5,26 +5,24 @@ declare(strict_types=1);
 namespace Gutocf\BrasilAPI\Tests\Adapter;
 
 use Gutocf\BrasilAPI\Adapter\AbstractAdapter;
+use Gutocf\BrasilAPI\Exception\BadRequestException;
+use Gutocf\BrasilAPI\Exception\InternalServerErrorException;
+use Gutocf\BrasilAPI\Exception\NotFoundException;
 use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
 use GuzzleHttp\Psr7\Uri;
 use PHPUnit\Framework\TestCase;
 
 class AbstractAdapterTest extends TestCase
 {
-    private AbstractAdapter $adapter;
-
-    public function setUp(): void
-    {
-        $this->adapter = $this->getMockForAbstractClass(AbstractAdapter::class, [new Client()]);
-    }
-
     public function testGetUri(): void
     {
-        $reflection = new \ReflectionClass(get_class($this->adapter));
+        $adapter = $this->getMockForAbstractClass(AbstractAdapter::class, [new Client()]);
+        $reflection = new \ReflectionClass(get_class($adapter));
         $method = $reflection->getMethod('getUri');
         $method->setAccessible(true);
         /** @var \GuzzleHttp\Psr7\Uri $uri */
-        $uri = $method->invoke($this->adapter, '/path/to/resource', [
+        $uri = $method->invoke($adapter, '/path/to/resource', [
             'foo' => 'bar',
             'baz' => 'qux',
             'foobar' => '',
@@ -32,5 +30,26 @@ class AbstractAdapterTest extends TestCase
         ]);
         $this->assertInstanceOf(Uri::class, $uri);
         $this->assertEquals('https://brasilapi.com.br/path/to/resource?foo=bar&baz=qux&foobar=', $uri->__toString());
+    }
+
+    public function testHandleErrors(): void
+    {
+        $adapter = $this->getMockForAbstractClass(AbstractAdapter::class, [new Client()]);
+
+        $reflection = new \ReflectionClass(get_class($adapter));
+        $method = $reflection->getMethod('handleErrors');
+        $method->setAccessible(true);
+
+        $this->expectException(BadRequestException::class);
+        $body = json_encode(['name' => 'Bad Request']);
+        $method->invoke($adapter, new Response(400, [], $body));
+
+        $this->expectException(NotFoundException::class);
+        $body = json_encode(['name' => 'Not Found']);
+        $method->invoke($adapter, new Response(404, [], $body));
+
+        $this->expectException(InternalServerErrorException::class);
+        $body = json_encode(['name' => 'Internal Server Error']);
+        $method->invoke($adapter, new Response(500, [], $body));
     }
 }
